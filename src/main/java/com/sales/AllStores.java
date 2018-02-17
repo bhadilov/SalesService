e16e2c43c99a6486082ed4cf24467ef5ba5e978f
Alec <alecnkhata@gmail.com>
2018-02-17 19:28:47 +0200

    added All Stores

---
+++ src/main/java/com/sales/AllStores.java
@@ -0,0 +1,31 @@
+package com.sales;
+
+public class AllStores {
+
+    private String Title,TitleUrl, ImageUrl, NewPrice, OldPrice, Discount;
+
+    public void setTitle(String salesTitle) {
+        this.Title = salesTitle;
+    }
+
+    public void setTitleUrl(String salesTitle) {
+        this.TitleUrl = salesTitle;
+    }
+
+    public void setImageUrl(String saleImageUrl) {
+        this.ImageUrl = saleImageUrl;
+    }
+
+    public void setNewPrice(String saleNewPrice) {
+        this.NewPrice = saleNewPrice;
+    }
+
+    public void setOldPrice(String saleOldPrice) {
+        this.OldPrice = saleOldPrice;
+    }
+
+    public void setDiscount(String saleDiscount) {
+        this.Discount = saleDiscount;
+    }
+
+}
